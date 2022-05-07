8f82f45f9c27aa1106635656de7ea2f3b87f4ea1
Irvin Camacho <irvin.acd@gmail.com>
2022-05-07 12:22:06 -0500

    Added a solution to a Codewars array problem.

--- 8 kyu/Arrays/Count of positives sum of negatives/main.js
+++ 8 kyu/Arrays/Count of positives sum of negatives/main.js
@@ -5,23 +5,19 @@
 //For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 function countPositivesSumNegatives(input) {
-  let count = 0;
-  for (let i = 0; i < input.length; i++) {
-      if (input[i] > 0) {
-        count += 1;
-    }
+  if (input == null || input.length == 0)
+    return [];
+  
+  let positiveCount = 0;
+  let negativeSum = 0;
+  
+  for (let i=0; i < input.length; i++)
+  {
+    if (input[i] > 0)
+      ++ positiveCount;
+    else
+      negativeSum += input[i];
   }
-  let sum = 0
-  for(let i = 0; i < input.length; i++){
-      if(input[i] < 0){
-          sum += input[i]
-      }
-  }
-  return [count,sum];
+  
+  return [positiveCount, negativeSum];
 }
-
-//Best practice:
-
-//function findNeedle(haystack) {
-//    return "found the needle at position " + haystack.indexOf("needle");
-//  }
