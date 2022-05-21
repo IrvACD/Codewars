28c662f1d0d193146274223ab8c759b4dd9ec0a9
Irvin Camacho <irvin.acd@gmail.com>
2022-05-20 23:56:49 -0500

    Added a new Codewars array problem solution.

---
+++ 8 kyu/Arrays/Count the Monkeys/main.js
@@ -0,0 +1,5 @@
+//You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+function monkeyCount(n) {
+  // your code here
+  }
