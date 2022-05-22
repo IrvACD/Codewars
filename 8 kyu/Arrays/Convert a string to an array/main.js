9bd3b6f43a8a913905089f0e94efb3a756cbdf15
Irvin Camacho <irvin.acd@gmail.com>
2022-05-22 11:30:54 -0500

    Added a new Codewars array problem solution.

--- 8 kyu/Arrays/Convert a string to an array/main.js
+++ 8 kyu/Arrays/Convert a string to an array/main.js
@@ -1,7 +1,10 @@
 //Write a function to split a string and convert it into an array of words.
+//Example: "Robin Singh" ==> ["Robin", "Singh"]
 
 function stringToArray(string){
-
-	// code code code
-
+	return string.split(' ');
 }
+
+//Explanation:
+//Notice how the spaces are considered characters in the return value.
+//If I wanted to change it so the string is split up into individual words, then the separator would be an empty string with a space.
--- 8 kyu/Arrays/Count the Monkeys/main.js
+++ 8 kyu/Arrays/Count the Monkeys/main.js
@@ -1,5 +1,13 @@
 //You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+//As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+//For example:
+//monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 function monkeyCount(n) {
-  // your code here
+  return Array.from({length: n}, (_, i) => i + 1)
   }
+
+//Explanation:
+//Array.from creates a new Array from an iterable object.
+//Here the "iterable" is just {length:n} then the created array will have a length of n.
+//The second (optional) argument is a map function wich will be called on each item of the new created array: (_,i)=>i+1). Here the item (_) is ignored, but new item is formed with index (i) + 1.
