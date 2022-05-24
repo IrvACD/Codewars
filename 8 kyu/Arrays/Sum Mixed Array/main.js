9ad5ffedcba9ce58c8c1c691df904293be95275f
Irvin Camacho <irvin.acd@gmail.com>
2022-05-23 19:48:40 -0500

    Added a new Codewars array problem solution.

---
+++ 8 kyu/Arrays/Sum Mixed Array/main.js
@@ -0,0 +1,7 @@
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+//Example:
+//['3', 6, 6, 0, '5', 8, 5, '6', 2,'0'] => 41
+
+function sumMix(x){
+
+}
