a2c27fedb78aab0d80bd0f603656b6c68e476913
Irvin Camacho <irvin.acd@gmail.com>
2022-06-08 19:22:06 -0500

    Added a new Codewars array problem solution.

--- 8 kyu/Arrays/Filter out the geese/main.js
+++ 8 kyu/Arrays/Filter out the geese/main.js
@@ -12,9 +12,16 @@
 //The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
 
 function gooseFilter (birds) {
-    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-    
-    
-  };
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
 
-  gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"])
+  const birdsToRemoveSet = new Set(geese);
+
+  let newArr = birds.filter((bird)=> {
+    return !birdsToRemoveSet.has(bird);
+  });
+
+  return newArr
+
+};
+
+gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"])
