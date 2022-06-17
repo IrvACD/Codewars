d6f71614122eac314c5f3490d898c5b7b6548276
Irvin Camacho <irvin.acd@gmail.com>
2022-06-16 22:48:14 -0500

    Added progress to the most recent Codewars array problem.

--- 8 kyu/Arrays/Swap Values/main.js
+++ 8 kyu/Arrays/Swap Values/main.js
@@ -10,3 +10,14 @@ function swapValues() {
 
     
 }
+
+describe("Tests", () => {
+    it("test", () => {
+  var arr = [1,2] 
+  swapValues(arr);
+  Test.assertEquals(arr[0], 2, "Failed swapping numbers")
+  Test.assertEquals(arr[1], 1, "Failed swapping numbers")
+  
+    });
+  });
+  
