4415c66622962da60c0bb07b09f3375f4712374d
Irvin Camacho <irvin.acd@gmail.com>
2022-06-17 23:50:42 -0500

    Added another attempt to solve the most my most recent Codewars array problem solution try.

--- 8 kyu/Arrays/Swap Values/main.js
+++ 8 kyu/Arrays/Swap Values/main.js
@@ -11,13 +11,5 @@ function swapValues() {
     
 }
 
-describe("Tests", () => {
-    it("test", () => {
-  var arr = [1,2] 
-  swapValues(arr);
-  Test.assertEquals(arr[0], 2, "Failed swapping numbers")
-  Test.assertEquals(arr[1], 1, "Failed swapping numbers")
-  
-    });
-  });
+var list1 = list(1, 2, 3)
   
