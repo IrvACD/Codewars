859428b9566183ee969513bebfe906319b74f72c
Irvin Camacho <irvin.acd@gmail.com>
2022-06-21 18:57:48 -0500

    Added a new Codewars array problem solution.

--- 8 kyu/Arrays/Merge two sorted arrays into one/main.js
+++ 8 kyu/Arrays/Merge two sorted arrays into one/main.js
@@ -16,3 +16,8 @@ function mergeArrays(arr1, arr2) {
     let cleanArray = [...new Set(mergedArrays)];
     return cleanArray.sort((a,b)=>a-b)
 }
+
+//BEST PRACTICE:
+//function mergeArrays(arr1, arr2) {
+//    return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
+//}
