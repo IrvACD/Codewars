2729ed567e69a08be8f19c31d13a5c1a87cd0006
Irvin Camacho <irvin.acd@gmail.com>
2022-07-04 16:54:44 -0500

    Added new completed tasks of Codeacademy JS course.

--- Codeacademy/Variables/Dog Years/main.js
+++ Codeacademy/Variables/Dog Years/main.js
@@ -5,4 +5,6 @@ let laterYears = myAge - 2;
 laterYears *= 4;
 let myAgeInDogYears = earlyYears + laterYears;
 
-console.log(myAgeInDogYears)
+myName = "Irvin".toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
