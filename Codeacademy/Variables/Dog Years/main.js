7b8735310ff549355da66e7a97ffbcb80e674f18
Irvin Camacho <irvin.acd@gmail.com>
2022-07-03 21:47:53 -0500

    Added a new Codeacademy begginer project.

---
+++ Codeacademy/Variables/Dog Years/main.js
@@ -0,0 +1,8 @@
+let myAge = 33; //created a variable
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+let myAgeInDogYears = earlyYears + laterYears;
+
+console.log(myAgeInDogYears)
