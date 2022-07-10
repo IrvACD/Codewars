3a0a4438671da873fbaebf1d837f2cd850e56d64
Irvin Camacho <irvin.acd@gmail.com>
2022-07-09 21:36:16 -0500

    Added a Codeacademy conditionals completed mini project.

--- Codeacademy/Conditionals/Project - Magic Eight Ball/main.js
+++ Codeacademy/Conditionals/Project - Magic Eight Ball/main.js
@@ -1,16 +1,37 @@
-let athleteFinalPosition = "first place";
+let userName = "";
+let userQuestion = "";
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = "";
 
-switch (athleteFinalPosition) {
-  case "first place":
-    console.log("You get the gold medal!");
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
+
+console.log(`Your question is "${userQuestion}".`);
+
+switch (randomNumber) {
+  case 0:
+    eightBall = "It is certain";
     break;
-  case "second place":
-    console.log("You get the silver medal!");
+  case 1:
+    eightBall = "It is decidedly so";
     break;
-  case "third place":
-    console.log("You get the bronze medal!");
+  case 2:
+    eightBall = "Reply hazy try again";
     break;
-  default:
-    console.log("No medal awarded.");
+  case 3:
+    eightBall = "Cannot predict now";
+    break;
+  case 4:
+    eightBall = "Do not count on it";
+    break;
+  case 5:
+    eightBall = "My sources say no";
+    break;
+  case 6:
+    eightBall = "Outlook not so good";
+    break;
+  case 7:
+    eightBall = "Signs point to yes";
     break;
 }
+
+console.log(`Magic Eight Ball answers "${eightBall}".`);
