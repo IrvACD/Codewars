980ce777a579eeb7d2b4b8f447170d5013b43556
Irvin Camacho <irvin.acd@gmail.com>
2022-07-12 23:30:30 -0500

    Added a new Codeacademy functions course completed task.

---
+++ Codeacademy/Functions/function expressions/main.js
@@ -0,0 +1,11 @@
+//In a function expression, the function name is usually omitted. A function with no name is called an anonymous function. A function expression is often stored in a variable in order to refer to it.
+
+const plantNeedsWater = function(day){
+    if(day === "Wednesday"){
+      return true
+    }else{
+      return false
+    }
+  }
+  
+  console.log(plantNeedsWater("Tuesday"))
