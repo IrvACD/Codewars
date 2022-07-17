2f536abf88c8984da0fc024b076a95f92ef76382
Irvin Camacho <irvin.acd@gmail.com>
2022-07-16 23:59:15 -0500

    Added Codeacademy scope task solution.

---
+++ Codeacademy/Scope/Blocks and Scope/main.js
@@ -0,0 +1,7 @@
+//A block is the code found inside a set of curly braces {}. Blocks help us group one or more statements together and serve as an important structural marker for our code.
+
+const city = "New York City"
+
+function logCitySkyline () {
+  let skyscraper = "Empire State Building"
+}
