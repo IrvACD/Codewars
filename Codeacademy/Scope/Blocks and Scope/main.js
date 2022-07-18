c90708b753f65f3bea7900da848ddecc3f33d9e8
Irvin Camacho <irvin.acd@gmail.com>
2022-07-17 22:33:36 -0500

    Added a new completed Codeacademy Scope task.

--- Codeacademy/Scope/Blocks and Scope/main.js
+++ Codeacademy/Scope/Blocks and Scope/main.js
@@ -4,4 +4,9 @@ const city = "New York City"
 
 function logCitySkyline () {
   let skyscraper = "Empire State Building"
+  return "The stars over the " + skyscraper + " in " + city;
 }
+
+console.log(logCitySkyline())
+
+//You’ll notice that the logCitySkyline() function is able to access both variables without any problems.
