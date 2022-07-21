a0590a529ad895af861a59f40422a054c5021a8b
Irvin Camacho <irvin.acd@gmail.com>
2022-07-21 15:34:36 -0500

    Added new Codeacademy Array tasks solutions.

---
+++ Codeacademy/Arrays/Accessing Elements/main.js
@@ -0,0 +1,15 @@
+//Each element in an array has a numbered position known as its index. We can access individual items using their index, which is similar to referencing an item in a list based on the item’s position.
+
+//Arrays in JavaScript are zero-indexed, meaning the positions start counting from 0 rather than 1. Therefore, the first item in an array will be at position 0.
+
+//Create a variable named listItem and set it equal to the first item in the famousSayings array using square bracket notation ([]).
+
+//Now, console.log() the third element in the famousSayings array using bracket notation to access the element.
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+let listItem = famousSayings[0]
+
+console.log(listItem)
+
+console.log(famousSayings[2])
---
+++ Codeacademy/Arrays/Create an Array/main.js
@@ -0,0 +1,5 @@
+//One way we can create an array is to use an array literal. An array literal creates an array by wrapping items in square brackets [].
+
+const hobbies = ['one', 'two', 'three']
+
+console.log(hobbies)
