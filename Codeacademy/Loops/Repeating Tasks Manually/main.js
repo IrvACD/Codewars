90631fb5fe3a7a58e16ba79953348ca102e6685f
IrvACD <irvin.acd@gmail.com>
2022-08-01 16:33:45 -0500

    Added a Codeacademy Loop task solution.

---
+++ Codeacademy/Loops/Repeating Tasks Manually/main.js
@@ -0,0 +1,7 @@
+//Before we write our own loops let’s take a moment to develop an appreciation for loops. The best way to do that is by showing you how cumbersome it would be if a repeated task required you to type out the same code every single time.
+
+let vacationSpots = ['One', 'Two', 'Three']
+
+console.log(vacationSpots[0])
+console.log(vacationSpots[1])
+console.log(vacationSpots[2])
