8013b281bee540b31bbf2d17f621545aa6c3e0cc
IrvACD <irvin.acd@gmail.com>
2022-08-22 13:45:30 -0500

    Added a Codeacademy objects task solution.

---
+++ Codeacademy/Objects/Creating Object Literals/main.js
@@ -0,0 +1,14 @@
+//We use curly braces, {}, to designate an object literal:
+
+let spaceship = {}; // spaceship is an empty object
+
+//This data is organized into key-value pairs.
+//We make a key-value pair by writing the key’s name, or identifier, followed by a colon and then the value.
+//We separate each key-value pair in an object literal with a comma (,).
+
+let spaceshipA = {
+  'Fuel type': 'diesel',
+  color: 'silver'
+};
+
+//'spaceshipA' is an object. 'Fuel type' is a key, and 'diesel' it's value.
