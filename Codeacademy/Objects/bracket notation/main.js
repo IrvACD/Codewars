f01d4c290391ed0b5920b3f493156b7bcae564e7
IrvACD <irvin.acd@gmail.com>
2022-08-23 18:32:06 -0500

    Added Codeacademy objects tasks solutions.

---
+++ Codeacademy/Objects/bracket notation/main.js
@@ -0,0 +1,13 @@
+//The second way to access a key’s value is by using bracket notation, [ ].
+
+let spaceship = {
+  'Fuel Type': 'Turbo Fuel',
+  'Active Duty': true,
+  homePlanet: 'Earth',
+  numCrew: 5
+};
+
+spaceship['Active Duty'];   // Returns true
+spaceship['Fuel Type'];   // Returns  'Turbo Fuel'
+spaceship['numCrew'];   // Returns 5
+spaceship['!!!!!!!!!!!!!!!'];   // Returns undefined
---
+++ Codeacademy/Objects/dot notation/main.js
@@ -0,0 +1,14 @@
+//There are two ways we can access an object’s property.
+//Let’s explore the first way— dot notation, '.'.
+
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
+let crewCount = spaceship.numCrew;
+let planetArray = spaceship.flightPath;
