bb23f4879492d9131591a05d425ca232bf394d54
IrvACD <irvin.acd@gmail.com>
2022-08-24 21:22:51 -0500

    Added a Codeacademy objects task solution.

--- Codeacademy/Objects/bracket notation/main.js
+++ Codeacademy/Objects/bracket notation/main.js
@@ -11,3 +11,8 @@ spaceship['Active Duty'];   // Returns true
 spaceship['Fuel Type'];   // Returns  'Turbo Fuel'
 spaceship['numCrew'];   // Returns 5
 spaceship['!!!!!!!!!!!!!!!'];   // Returns undefined
+
+//With bracket notation you can also use a variable inside the brackets to select the keys of an object. This can be especially helpful when working with functions:
+
+let returnAnyProp = (objectName, propName) => objectName[propName];
+returnAnyProp(spaceship, 'homePlanet'); // Returns 'Earth'
